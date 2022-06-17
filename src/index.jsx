8a1c4d5e2692b7c7fe99ab8fa56cdf9582db092b
victor_cordova <victorcordovapoma@gmail.com>
2022-06-17 11:22:37 -0500

    Saved a file.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,5 @@
 import ReactDOM from "react-dom";
 import {App} from "./components/App";
 import "./styles/main.css"
-import inversion from "./static/images/primera-inversion.jpeg"
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
-
-// const image = document.createElement("img");
